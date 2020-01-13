3976601659b529eb5969662cd78bf56994b1f5a2
xabab-pc <14582595+Xabab@users.noreply.github.com>
2020-01-13 07:02:11 +0200

    added Jessica Fridrich class dummy

--- logic/JessicaFridrich.py
+++ logic/JessicaFridrich.py
@@ -12,11 +12,12 @@
 #
 #      You should have received a copy of the GNU General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from logic.KochZhao import KochZhao
 
+from logic.KochZhao import KochZhao
+from logic.util.dctEssentials import *
 
 class JessicaFridrich(KochZhao):
     def __init__(self):
         super().__init__()
 
-        self.
+        raise NotImplementedError
